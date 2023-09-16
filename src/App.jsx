003e34d5ee28bcc327a80d528cc7698beaaed0ea
Santosh <santoshkumar23kky@gmail.com>
2023-09-16 12:19:24 +0530

    ViteStartup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className='flex justify-center text-4xl font-medium bg-gray-900 text-white p-3 '>Hello Santosh</div>
+    </>
+  )
+}
+
+export default App
